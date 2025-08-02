e7aa60375a8c1c816c8d7e9eda94956b2ed1c9ac
ArrBrants <acelemur2589866@outlook.com>
2025-08-02 12:45:26 +0800

    Updated EFAB in activity_main.xml

--- app/src/main/java/dev/axiom/MainActivity.java
+++ app/src/main/java/dev/axiom/MainActivity.java
@@ -5,19 +5,23 @@ import dev.axiom.databinding.ActivityMainBinding;
 
 public class MainActivity extends BaseActivity {
     private ActivityMainBinding binding;
-	
     @Override
     public void onCreateDoes() {
 		binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         setSupportActionBar(binding.toolbar);
-		binding.fab.setOnClickListener(v ->
-          Toast.makeText(MainActivity.this, "Replace with your action", Toast.LENGTH_SHORT).show()
+		binding.efab.setOnClickListener(v ->
+          fabOnClickEvents()
         );
     }
     @Override
     protected void onDestroy() {
         super.onDestroy();
         this.binding = null;
+    }
+    private void fabOnClickEvents(){
+        if (binding.efab.isExtended()) binding.efab.shrink();
+        else binding.efab.extend();
+        Toast.makeText(MainActivity.this, "Toast", Toast.LENGTH_SHORT).show();
     }
 }
